4a703aa407c431ed931b72cd6c5d287b9f90e9fc
MEG HOWE <howem9@gmail.com>
2021-07-21 13:59:08 -0400

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Sample from "../views/Sample.vue";
+import Signup from "../views/Signup.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/sample",
+    name: "Sample",
+    component: Sample,
+  },
+  { path: "/signup", name: "signup", component: Signup },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
