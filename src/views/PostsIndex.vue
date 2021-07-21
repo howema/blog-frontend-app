9032c631712c281b35a4d7ad30820cb162949f12
MEG HOWE <howem9@gmail.com>
2021-07-21 14:59:10 -0400

    Adds Index and connects to backend, adding the posts

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="postsindex">
+    <h1>Posts Index</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h2>Title: {{ post.title }} </h2>
+      <p>Body: {{ post.body }} </p>
+      <img v-bind:src="post.image" alt="post.title" />
+      <button>Buttonz!</button>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Here are posts",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.postsIndex();
+  },
+  methods: {
+    postsIndex: function () {
+      axios.get("http://localhost:3000/posts").then((response) => {
+        this.posts = response.data;
+        console.log("All posts:", this.posts);
+      });
+    },
+  },
+};
+</script>
